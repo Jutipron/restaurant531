b39ea44554646ca5f4818fa3c6d58d6e589f7cf2
Jutipron <124545898+Jutipron@users.noreply.github.com>
2023-05-29 12:09:03 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,146 @@
+var tourpoint = 
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "5",
+        "name": "นุสรา"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49166028978044,
+          13.745524773704858
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "5",
+        "name": "ศรณ์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56852927116341,
+          13.723250366955682
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "3",
+        "name": "เรือนมัลลิการ์"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56309300000248,
+          13.725352675209194
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "4",
+        "name": "บ้านขนิษฐา"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56342399999835,
+          13.741377507475619
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "3",
+        "name": "ลูกไก่ทอง"
+      },
+      "geometry": {
+        "coordinates": [
+          100.53421981349089,
+          13.747170502889588
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "5",
+        "name": "มุมอร่อย"
+      },
+      "geometry": {
+        "coordinates": [
+          100.91122627116329,
+          12.978869004249859
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "3",
+        "name": "อรุณวาณิช"
+      },
+      "geometry": {
+        "coordinates": [
+          99.89710926934788,
+          12.973439730013439
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "4",
+        "name": "ร้าน ธธดา TH•TH•DAH"
+      },
+      "geometry": {
+        "coordinates": [
+          100.71337618107447,
+          13.950760534043297
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "3",
+        "name": "ระเบียง ณ แม่ริม"
+      },
+      "geometry": {
+        "coordinates": [
+          98.93528727116654,
+          18.923058248429285
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Level": "3",
+        "name": "โกปี๊ เดอ ภูเก็ต "
+      },
+      "geometry": {
+        "coordinates": [
+          98.39114461508223,
+          7.882547309874937
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
